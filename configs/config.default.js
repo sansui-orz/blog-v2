33ba4bb53d79c7cf319dc909f8dd55a003d8dcfd
雪人 <xueren@i9i8.com>
2021-09-18 16:03:39 +0800

    blog-oss打包脚本升级

--- configs/config.default.js
+++ configs/config.default.js
@@ -1,7 +1,10 @@
 module.exports = {
   // 博客基础信息
-  name: 'Michael\'s blog',
-  selogen: '学如逆水行舟，不进则退',
+  name: '前端于我',
+  slogen: '咸鱼前端的自我修养',
+  favicon: 'https://limaosheng.top/b.png',
+  ICPCode: '粤ICP备2021113739号-1',
+  githubAddress: 'https://github.com/sansui-orz/blog-v2/',
   github: {
     clientID: '9568d133f79d7a39aa32',
     clientSecret: 'a7c8fcf003cdb377cfc7d891493bea8cf1d48816',
